cef244b0b7005136e273620cc20222b0348b07d1
Vishnu <857006+vkkong@users.noreply.github.com>
2024-09-07 15:06:57 -0400

    Remvoe the extra empty div on page load.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { FolderTree } from "./FolderTree";
-import { ChatHistory } from "./ChatHistory";
-import { SendChatContainer } from "./SendChatContainer";
-import { ChatProvider } from '../context/ChatContext';
-import { FolderProvider } from '../context/FolderContext';
-import { StreamedContent } from './StreamedContent';
+import {FolderTree} from "./FolderTree";
+import {ChatHistory} from "./ChatHistory";
+import {SendChatContainer} from "./SendChatContainer";
+import {useChatContext} from '../context/ChatContext';
+import {StreamedContent} from './StreamedContent';
 
 export const App = () => {
+    const {streamedContent, messages} = useChatContext();
     return (
-        <ChatProvider>
-            <FolderProvider>
-                <h2 style={{textAlign: "center", marginBlock: '0.5em'}}>Ziya: Code Assist</h2>
-                <div className="container">
-                    <FolderTree/>
-                    <SendChatContainer/>
+        <>
+            <h2 style={{textAlign: "center", marginBlock: '0.5em'}}>Ziya: Code Assist</h2>
+            <div className="container">
+                <FolderTree/>
+                <SendChatContainer/>
+                {(messages.length > 0 || streamedContent) && (
                     <div className="chat-container">
                         <StreamedContent/>
                         <ChatHistory/>
-                    </div>
-                </div>
-            </FolderProvider>
-        </ChatProvider>
+                    </div>)}
+            </div>
+        </>
     );
 };
