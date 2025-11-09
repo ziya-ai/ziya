a792ad3e37aea39d20b663b7539a2b82a32d03b4
Dan Cohn <dcohn@amazon.com>
2025-11-09 09:43:03 -0800

    cleanup root detritus

--- test-puppeteer.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  console.log('Page title:', await page.title());
-  await browser.close();
-})();
